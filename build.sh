d7bc6676fe6528c3ba9e8d355bf43df21a0130e2
Lucas Mazzetto <lucasrmazzetto@gmail.com>
2024-07-09 19:29:27 -0300

    Add convenience scripts for building and running the Docker container

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t modelsim:20.1 .
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if program and project path are provided
+if [ -z "$1" ]; then
+  echo "Usage: ./run.sh <project_path>"
+  exit 1
+fi
+
+# Set project path
+PROJECT_PATH=$1
+
+# Run the Docker container
+docker run -it --net=host --ipc=host \
+           -e DISPLAY=$DISPLAY \
+           -e XAUTHORITY=/tmp/.XAuthority \
+           -v /tmp/.X11-unix:/tmp/.X11-unix \
+           -v $XAUTHORITY:/tmp/.XAuthority \
+           -v ${PROJECT_PATH}:/home/docker/project \
+           -u $(id -u):$(id -g) \
+           modelsim-20.1
